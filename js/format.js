a5ab35ec225f032426e2b863ddd54c113cdc82e9
Piotr Krzysztof Wyrwas <pw.piotr.wyrwas@gmail.com>
2022-03-20 13:15:06 +0100

    Add menu table

---
+++ js/format.js
@@ -0,0 +1,11 @@
+function tab_pic(url) {
+    return "<img src='" + url + "' class='pic-small pic-tab'>";
+}
+
+function price(n) {
+    return "$" + n + ",-";
+}
+
+function price_dec(n) {
+    return "$" + n;
+}
